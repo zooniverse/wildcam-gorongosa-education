65d733cd6990567e2de2cfaf73ffe68aeaaf0c29
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-01-06 16:32:04 +0000

    Elements now use display: flex. Website fits the window, at minimum. Works on Chrome, FF, Opera, Edge, but NOT IE11. This is being investigated.

---
+++ src/components/MapExplorer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <div id="map-explorer">
+        <h2>Map Explorer</h2>
+        <p>THIS PAGE SHOULD EXPAND TO FIT THE WINDOW ...minus any header/footer.</p>
+      </div>
+    );
+  }
+});
