12a00b1d1ddf339c3fe15bbe3f6187b49b50d5b2
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-01-06 16:32:04 +0000

    Elements now use display: flex. Website fits the window, at minimum. Works on Chrome, FF, Opera, Edge, but NOT IE11. This is being investigated.

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 
 import App from './components/App.jsx';
+import MapExplorer from './components/MapExplorer.jsx';
 import PoweredBy from './components/Powered-by.jsx';
 import About from './components/About.jsx';
 
@@ -14,8 +15,9 @@ ReactDOM.render(
   <Router>
     <Route path="/" component={App}>
       <Route path="/about" component={About}/>
+      <Route path="/map" component={MapExplorer}/>
       <Route path="/poweredby" component={PoweredBy}/>
     </Route>
   </Router>
-  , document.getElementById('root')
+  , document.getElementById('app-container')
 );
