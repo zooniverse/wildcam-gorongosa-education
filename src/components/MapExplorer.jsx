27c50704efac646e863d0a3c273ea770b17380a3
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-01-11 20:34:00 +0000

    Google Maps API (and requisite ext. script loader) now functioning in React Google Maps API now functions in React. Key requirement was getting an external library loaded dynamically in React. This was achieved by using an external plugin; "react-loadscript".

--- src/components/MapExplorer.jsx
+++ src/components/MapExplorer.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import {Script} from 'react-loadscript';
 
 export default React.createClass({
+  
+  initMapExplorer() {
+    window.map = new google.maps.Map(this.refs.mapVisuals, {
+      center: {lat: -18.9754, lng: 34.5158},
+      zoom: 11
+    });
+  },
+
   render() {
+    window.initMapExplorer = this.initMapExplorer;
+    
     return (
       <div className="map-explorer">
-        <h2>Map Explorer</h2>
-        <p>THIS PAGE SHOULD EXPAND TO FIT THE WINDOW ...minus any header/footer.</p>
+        <div ref="mapVisuals" className="map-visuals"></div>
+        <div className="map-controls">
+          <Script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAzMva99KALzUQfe_BCkCovZyGK-dld08s&callback=initMapExplorer'>{
+            ({done}) => !done ? <div>Google Maps API is loading</div> : <div>Google Maps is ready</div>
+          }</Script>
+        </div>
       </div>
     );
+  },
+  
+  componentDidMount() {
+    if (window.google && window.google.maps) {
+      console.log("componentDidMount: window.google found.");
+      this.initMapExplorer();
+    } else {
+      console.log("componentDidMount: window.google not found.");
+    }
   }
 });
+
+//<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzMva99KALzUQfe_BCkCovZyGK-dld08s&callback=initMapExplorer"></script>
