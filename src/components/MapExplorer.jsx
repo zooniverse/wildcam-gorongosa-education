19ff29068ad779fb58f8edc9260512e712fca254
Roger Hutchings <roger@hutchin.gs>
2016-01-20 15:15:52 +0000

    Slightly refactor Map stuff
    Now uses ES6, and loads in static data from a JSON file

--- src/components/MapExplorer.jsx
+++ src/components/MapExplorer.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import {Script} from 'react-loadscript';
-var Cameras = require('../data/Cameras.jsx');
+
+import Cameras from '../data/Cameras.jsx';
 
 export default React.createClass({
-  
-  //The Google API key ending with dld08s ("dildos") is tied to the
-  //shaun.a.noordin@zooniverse.org account and is only used for development.
-  GOOGLE_MAPS_API_KEY: "AIzaSyAzMva99KALzUQfe_BCkCovZyGK-dld08s",
 
-  googleMap: undefined,
-  
-  getInitialState: function() {
-    this.googleMap = undefined;    
-    return {
-    };
+  //The Google API key ending with dld08s ('dildos') is tied to the
+  //shaun.a.noordin@zooniverse.org account and is only used for development.
+  GOOGLE_MAPS_API_KEY: 'AIzaSyAzMva99KALzUQfe_BCkCovZyGK-dld08s',
+
+  googleMap: void 0,
+
+  getInitialState: () => {
+    this.googleMap = void 0;
+    return {};
   },
-  
+
   render() {
     window.initMapExplorer = this.initMapExplorer;
-    
+    let googleMapScript = 'https://maps.googleapis.com/maps/api/js?key=${this.GOOGLE_MAPS_API_KEY}&callback=initMapExplorer';
+
     return (
-      <div className="map-explorer">
-        <div ref="mapVisuals" className="map-visuals"></div>
-        <div className="map-controls">
-          <Script src={'https://maps.googleapis.com/maps/api/js?key='+this.GOOGLE_MAPS_API_KEY+'&callback=initMapExplorer'}>{
+      <div className='map-explorer'>
+        <div ref='mapVisuals' className='map-visuals'></div>
+        <div className='map-controls'>
+          <Script src={googleMapScript}>{
             ({done}) => !done ? <div>Google Maps API is loading</div> : <div>Google Maps is ready</div>
           }</Script>
         </div>
@@ -35,21 +36,21 @@ export default React.createClass({
   //See notes on initMapExlorer().
   componentDidMount() {
     if (window.google && window.google.maps) {
-      console.log("componentDidMount: window.google found.");
+      console.log('componentDidMount: window.google found.');
       this.initMapExplorer();
     } else {
-      console.log("componentDidMount: window.google not found.");
+      console.log('componentDidMount: window.google not found.');
     }
   },
-  
+
   //Cleanup!
   componentWillUnmount() {
     //Note that the Map Explorer (and hence, the Google Map) is recreated every
     //time the Map page is opened/navigated to. As such, we want to make sure we
     //wipe all references/handles to prevent a memory leak.
-    this.googleMap = undefined;
+    this.googleMap = void 0;
   },
-  
+
   //Initialises the Map Explorer.
   //NOTE: initMapExlorer() can be called in two ways:
   //1. When the map is loaded for the first time, the <Script> for the Google
@@ -60,33 +61,36 @@ export default React.createClass({
   //   called during componentDidMount(), after the necessary HTML elements have
   //   been rendered.
   initMapExplorer() {
-    console.log("MapExplorer.initMapExplorer()");
-    
+    console.log('MapExplorer.initMapExplorer()');
+
     this.googleMap = new google.maps.Map(this.refs.mapVisuals, {
-      center: { lat: Cameras.median.lat, lng: Cameras.median.lng },
+      center: {
+        lat: Cameras.median.lat,
+        lng: Cameras.median.lng
+      },
       zoom: 11
     });
-    
+
     this.paintAllCameras();
   },
-  
+
   //PLACEHOLDER - shaun.a.noordin@zooniverse.org to do something about this.
   //Like, make it a proper function with user-selected filters.
   paintAllCameras() {
-    console.log("MapExplorer.paintAllCameras()");
-    
+    console.log('MapExplorer.paintAllCameras()');
+
     //Sanity Check
     if (!(window.google && window.google.maps && this.googleMap)) {
-      console.log("ERROR: MapExplorer.paintAllCameras() doesn't have a map to paint on!");
-      return;  
+      console.log('ERROR: MapExplorer.paintAllCameras() doesn\'t have a map to paint on!');
+      return;
     }
-    
-    var inputColour = "#f39";
-    var inputOpacity = 0.5;
-    var inputSize = 500;
-    
-    for (var i = 0, camera; camera = Cameras.all[i]; i++) {
-      var marker = new google.maps.Circle({
+
+    let inputColour = '#f39';
+    let inputOpacity = 0.5;
+    let inputSize = 500;
+
+    for (let i = 0, camera; camera = Cameras.all[i]; i++) {
+      let marker = new google.maps.Circle({
         center: {
           lat: parseFloat(camera.latitude),
           lng: parseFloat(camera.longitude)
@@ -98,16 +102,16 @@ export default React.createClass({
         strokeWeight: 0,
         map: this.googleMap
       });
-      
+
       marker.camera = camera;
-      
-      marker.addListener('click', function() {
+
+      marker.addListener('click', () => {
         alert(this.camera.ID);
         this.googleMap.setCenter(this.getPosition());
         this.googleMap.setZoom(12);
-      }.bind(marker));
+      });
     }
-    
+
   }
-  
+
 });
