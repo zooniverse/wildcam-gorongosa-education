99c207f77b212f166ed7cd0a5681b3572276ef5d
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-01-26 17:53:18 +0000

    Functional CartoDB prototype with access to SQL and CSS layers.

--- src/components/MapExplorer.jsx
+++ src/components/MapExplorer.jsx
@@ -4,12 +4,23 @@ var Cameras = require('../data/Cameras.jsx');
 
 export default React.createClass({
 
+  cartodbVis: undefined,
   cartodbMap: undefined,
+  cartodbLayers: undefined,  //Array of map layers. layer[0] is the base (cartographic map).
+  cartodbDataLayer: undefined,
+  
+  //All cartodbConfig
+  cartodbConfig: {
+    mapVisualisationUrl: 'https://shaunanoordin-zooniverse.cartodb.com/api/v2/viz/e04c2e20-a8a9-11e5-8d6b-0e674067d321/viz.json',
+    dataTable: 'wildcam_gorongosa_cameras_201512',
+    dataLayerIndex: 1
+  },
 
-  getInitialState: function() {
+  getInitialState() {
+    this.cartodbVis = undefined;
     this.cartodbMap = undefined;
-    return {
-    };
+    this.cartodbLayers = undefined;
+    return {};
   },
 
   render() {
@@ -21,8 +32,19 @@ export default React.createClass({
         <div ref="mapVisuals" id="mapVisuals" className="map-visuals"></div>
         <div ref="mapControls" className="map-controls">
           <Script src={'http://libs.cartocdn.com/cartodb.js/v3/3.15/cartodb.js'}>{
-            ({done}) => !done ? <div>Google Maps API is loading</div> : this.initMapExplorer()
+            ({done}) => !done ? <div className="message">CartoDB API is loading</div> : this.initMapExplorer()
           }</Script>
+          <div className="inputRow">
+            <label>Map SQL</label>
+            <textarea ref="mapSql"></textarea>
+          </div>
+          <div className="inputRow">
+            <label>Map CSS</label>
+            <textarea ref="mapCss"></textarea>
+          </div>
+          <div className="inputRow">
+            <button onClick={this.updateMapExplorer}>Update</button>
+          </div>
         </div>
       </div>
     );
@@ -41,31 +63,80 @@ export default React.createClass({
 
   //Cleanup!
   componentWillUnmount() {
+    this.cartodbVis = undefined;
     this.cartodbMap = undefined;
+    this.cartodbLayers = undefined;
   },
 
   //Initialises the Map Explorer.
   //NOTE: initMapExlorer() can be called in two ways:
   //1. When the map is loaded for the first time, the <Script> for the  API
   //   needs to be loaded dynamically. When it's successfully loaded,
-  //   initMapExplorer is called back.
+  //   initMapExplorer is called.
   //2. If the <Script> has been loaded previously - e.g. the user navigated
   //   away from the page and has now returned - then initMapExplorer() will be
   //   called during componentDidMount(), after the necessary HTML elements have
   //   been rendered.
   initMapExplorer() {
     console.log('MapExplorer.initMapExplorer()');
-    
-    console.log(this.refs.mapVisuals);
-    console.log(document.getElementById('mapVisuals'));
-    
     this.refs.mapVisuals.style.height = '1000px';
-    cartodb.createVis('mapVisuals', 'https://shaunanoordin-zooniverse.cartodb.com/api/v2/viz/e04c2e20-a8a9-11e5-8d6b-0e674067d321/viz.json');
-    
+    cartodb.createVis('mapVisuals', this.cartodbConfig.mapVisualisationUrl)
+      .done(function (vis, layers) {
+        this.cartodbVis = vis;
+        this.cartodbMap = vis.getNativeMap();
+        this.cartodbLayers = layers;
+        if (layers.length >= (this.cartodbConfig.dataLayerIndex+1)) {
+          this.cartodbDataLayer = layers[this.cartodbConfig.dataLayerIndex];
+        }
+      }.bind(this));
     this.resizeMapExplorer();
-    return <div>READY</div>;
+    
+    this.refs.mapSql.value = 'SELECT * FROM "' + this.cartodbConfig.dataTable + '" WHERE "veg_type" LIKE \'Mixed Savanna and Woodland\' OR "veg_type" LIKE \'Floodplain Grassland\'';
+    this.refs.mapCss.value = [
+      '#'+this.cartodbConfig.dataTable+' {',
+      '  marker-fill: #fc3;',
+      '  marker-fill-opacity: 0.9;',
+      '  marker-line-color: #FFF;',
+      '  marker-line-width: 2;',
+      '  marker-line-opacity: 1;',
+      '  marker-placement: point;',
+      '  marker-type: ellipse;',
+      '  marker-width: 20;',
+      '  marker-allow-overlap: true;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 300] {',
+      '  marker-width: 30;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 200] {',
+      '  marker-width: 40;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 100] {',
+      '  marker-width: 50;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[veg_type="Floodplain Grassland"] {',
+      '  marker-fill: #3ff;',
+      '}'
+    ].join('\n');
+    
+    return <div className="message">CartoDB API READY</div>;
 
     //this.paintAllCameras();
+  },
+  
+  updateMapExplorer(e) {
+    
+    //Delete everything.
+    //TODO: This is a temporary measure to get a clean start! It's not really
+    //necessary to delete and rebuild layers - we can modify existing ones,
+    //assuming we can figure out which existing ones we want to twiddle with.
+    for (let i = this.cartodbDataLayer.getSubLayerCount() - 1; i >= 0; i--) {
+      this.cartodbDataLayer.getSubLayer(i).remove();
+    }
+    
+    this.cartodbDataLayer.createSubLayer({
+      sql: this.refs.mapSql.value,
+      cartocss: this.refs.mapCss.value
+    });
   },
   
   resizeMapExplorer() {
@@ -79,41 +150,6 @@ export default React.createClass({
   //PLACEHOLDER - shaun.a.noordin@zooniverse.org to do something about this.
   //Like, make it a proper function with user-selected filters.
   paintAllCameras() {
-    console.log('MapExplorer.paintAllCameras()');
-
-    //Sanity Check
-    if (!(window.google && window.google.maps && this.cartodbMap)) {
-      console.log('ERROR: MapExplorer.paintAllCameras() doesn\'t have a map to paint on!');
-      return;
-    }
-
-    var inputColour = '#f39';
-    var inputOpacity = 0.5;
-    var inputSize = 500;
-
-    for (var i = 0, camera; camera = Cameras.all[i]; i++) {
-      var marker = new google.maps.Circle({
-        center: {
-          lat: parseFloat(camera.latitude),
-          lng: parseFloat(camera.longitude)
-        },
-        radius: inputSize,
-        fillColor: inputColour,
-        fillOpacity: inputOpacity,
-        strokeColor: '#fff',
-        strokeWeight: 0,
-        map: this.cartodbMap
-      });
-
-      marker.camera = camera;
-
-      marker.addListener('click', function() {
-        alert(this.camera.ID);
-        this.cartodbMap.setCenter(this.getPosition());
-        this.cartodbMap.setZoom(12);
-      }.bind(marker));
-    }
-
   }
 
 });
