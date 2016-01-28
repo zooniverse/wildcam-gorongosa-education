0d00a5b597ef1def7c6859447da4e3bd95f75954
Roger Hutchings <roger@hutchin.gs>
2016-01-28 02:33:55 +0000

    Trash starterify components

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 
+import Admin from './components/Admin.jsx';
 import App from './components/App.jsx';
 import MapExplorer from './components/MapExplorer.jsx';
-import PoweredBy from './components/Powered-by.jsx';
-import About from './components/About.jsx';
-import Admin from './components/Admin.jsx';
-
 import Styles from './styles/main.styl'
 
 window.React = React;
@@ -15,9 +12,7 @@ window.React = React;
 ReactDOM.render(
   <Router>
     <Route path="/" component={App}>
-      <Route path="/about" component={About}/>
       <Route path="/map" component={MapExplorer}/>
-      <Route path="/poweredby" component={PoweredBy}/>
       <Route path="/admin" component={Admin}/>
     </Route>
   </Router>
