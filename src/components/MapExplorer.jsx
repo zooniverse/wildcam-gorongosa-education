eb435e97e69baf0a52ea7c14c26873cf3699963f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-02-03 11:37:01 +0000

    Rebased cartoDB prototype to the latest version of master.

--- src/components/MapExplorer.jsx
+++ src/components/MapExplorer.jsx
@@ -1,38 +1,47 @@
 import React from 'react';
-import { Script } from 'react-loadscript';
-
-import Cameras from '../data/Cameras.jsx';
-
+import {Script} from 'react-loadscript';
+var Cameras = require('../data/Cameras.jsx');
 
 export default React.createClass({
+  cartodbVis: undefined,
+  cartodbMap: undefined,
+  cartodbLayers: undefined,  //Array of map layers. layer[0] is the base (cartographic map).
+  cartodbDataLayer: undefined,
+  
+  //All cartodbConfig
+  cartodbConfig: {
+    mapVisualisationUrl: 'https://shaunanoordin-zooniverse.cartodb.com/api/v2/viz/e04c2e20-a8a9-11e5-8d6b-0e674067d321/viz.json',
+    dataTable: 'wildcam_gorongosa_cameras_201601',
+    dataLayerIndex: 1
+  },
 
-  //The Google API key ending with dld08s ('dildos') is tied to the
-  //shaun.a.noordin@zooniverse.org account and is only used for development.
-  GOOGLE_MAPS_API_KEY: 'AIzaSyAzMva99KALzUQfe_BCkCovZyGK-dld08s',
-  GOOGLE_MAPS_API_VERSION: '3',
-
-  googleMap: undefined,
-
-  getInitialState: function() {
-    this.googleMap = undefined;
+  getInitialState() {
+    this.cartodbVis = undefined;
+    this.cartodbMap = undefined;
+    this.cartodbLayers = undefined;
     return {};
   },
 
   render() {
-    window.initMapExplorer = this.initMapExplorer;
-
-    let googleMapScriptUrl = 'https://maps.googleapis.com/maps/api/js' +
-      `?key=${this.GOOGLE_MAPS_API_KEY}` +
-      `&v=${this.GOOGLE_MAPS_API_VERSION}` +
-      `&callback=initMapExplorer`;
-
     return (
       <div className='map-explorer'>
-        <div ref='mapVisuals' className='map-visuals'></div>
-        <div className='map-controls'>
-          <Script src={googleMapScriptUrl}>{
-            ({done}) => !done ? <div>Google Maps API is loading</div> : <div>Google Maps is ready</div>
+        <link rel="stylesheet" href="http://libs.cartocdn.com/cartodb.js/v3/3.15/themes/css/cartodb.css" />
+        <div ref="mapVisuals" id="mapVisuals" className="map-visuals"></div>
+        <div ref="mapControls" className="map-controls">
+          <Script src={'http://libs.cartocdn.com/cartodb.js/v3/3.15/cartodb.js'}>{
+            ({done}) => !done ? <div className="message">CartoDB API is loading</div> : this.initMapExplorer()
           }</Script>
+          <div className="inputRow">
+            <label>Map SQL</label>
+            <textarea ref="mapSql"></textarea>
+          </div>
+          <div className="inputRow">
+            <label>Map CSS</label>
+            <textarea ref="mapCss"></textarea>
+          </div>
+          <div className="inputRow">
+            <button onClick={this.updateMapExplorer}>Update</button>
+          </div>
         </div>
       </div>
     );
@@ -41,83 +50,99 @@ export default React.createClass({
   //Once React component has rendered, process the map.
   //See notes on initMapExlorer().
   componentDidMount() {
-    if (window.google && window.google.maps) {
-      console.log('componentDidMount: window.google found.');
+    if (window.cartodb) {
+      console.log('componentDidMount: window.cartodb found.');
       this.initMapExplorer();
     } else {
-      console.log('componentDidMount: window.google not found.');
+      console.log('componentDidMount: window.cartodb not found.');
     }
   },
 
   //Cleanup!
   componentWillUnmount() {
-    //Note that the Map Explorer (and hence, the Google Map) is recreated every
-    //time the Map page is opened/navigated to. As such, we want to make sure we
-    //wipe all references/handles to prevent a memory leak.
-    this.googleMap = undefined;
+    this.cartodbVis = undefined;
+    this.cartodbMap = undefined;
+    this.cartodbLayers = undefined;
   },
 
   //Initialises the Map Explorer.
   //NOTE: initMapExlorer() can be called in two ways:
-  //1. When the map is loaded for the first time, the <Script> for the Google
-  //   Maps API needs to be loaded dynamically. When it's successfully loaded,
-  //   initMapExplorer is called back.
+  //1. When the map is loaded for the first time, the <Script> for the  API
+  //   needs to be loaded dynamically. When it's successfully loaded,
+  //   initMapExplorer is called.
   //2. If the <Script> has been loaded previously - e.g. the user navigated
   //   away from the page and has now returned - then initMapExplorer() will be
   //   called during componentDidMount(), after the necessary HTML elements have
   //   been rendered.
   initMapExplorer() {
     console.log('MapExplorer.initMapExplorer()');
-
-    this.googleMap = new google.maps.Map(this.refs.mapVisuals, {
-      center: {
-        lat: Cameras.median.lat,
-        lng: Cameras.median.lng
-      },
-      zoom: 11
+    this.refs.mapVisuals.style.height = '1000px';
+    cartodb.createVis('mapVisuals', this.cartodbConfig.mapVisualisationUrl)
+      .done(function (vis, layers) {
+        this.cartodbVis = vis;
+        this.cartodbMap = vis.getNativeMap();
+        this.cartodbLayers = layers;
+        if (layers.length >= (this.cartodbConfig.dataLayerIndex+1)) {
+          this.cartodbDataLayer = layers[this.cartodbConfig.dataLayerIndex];
+        }
+      }.bind(this));
+    this.resizeMapExplorer();
+    
+    this.refs.mapSql.value = 'SELECT * FROM "' + this.cartodbConfig.dataTable + '" WHERE "veg_type" LIKE \'Mixed Savanna and Woodland\' OR "veg_type" LIKE \'Floodplain Grassland\'';
+    this.refs.mapCss.value = [
+      '#'+this.cartodbConfig.dataTable+' {',
+      '  marker-fill: #fc3;',
+      '  marker-fill-opacity: 0.9;',
+      '  marker-line-color: #FFF;',
+      '  marker-line-width: 2;',
+      '  marker-line-opacity: 1;',
+      '  marker-placement: point;',
+      '  marker-type: ellipse;',
+      '  marker-width: 20;',
+      '  marker-allow-overlap: true;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 300] {',
+      '  marker-width: 30;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 200] {',
+      '  marker-width: 40;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[dist_water_m < 100] {',
+      '  marker-width: 50;',
+      '}',
+      '#'+this.cartodbConfig.dataTable+'[veg_type="Floodplain Grassland"] {',
+      '  marker-fill: #3ff;',
+      '}'
+    ].join('\n');
+    
+    return <div className="message">CartoDB API READY</div>;
+  },
+  
+  updateMapExplorer(e) {
+    //Delete everything.
+    //TODO: This is a temporary measure to get a clean start! It's not really
+    //necessary to delete and rebuild layers - we can modify existing ones,
+    //assuming we can figure out which existing ones we want to twiddle with.
+    for (let i = this.cartodbDataLayer.getSubLayerCount() - 1; i >= 0; i--) {
+      this.cartodbDataLayer.getSubLayer(i).remove();
+    }
+    
+    this.cartodbDataLayer.createSubLayer({
+      sql: this.refs.mapSql.value,
+      cartocss: this.refs.mapCss.value
     });
-
-    this.paintAllCameras();
+  },
+  
+  resizeMapExplorer() {
+    let windowHeight = window.innerHeight;
+    let headerHeight = document.getElementsByClassName('site-header')[0].offsetHeight;
+    let footerHeight = document.getElementsByClassName('site-footer')[0].offsetHeight;
+    let availableHeight = windowHeight - headerHeight - footerHeight;
+    this.refs.mapVisuals.style.height = availableHeight+'px';
   },
 
   //PLACEHOLDER - shaun.a.noordin@zooniverse.org to do something about this.
   //Like, make it a proper function with user-selected filters.
   paintAllCameras() {
-    console.log('MapExplorer.paintAllCameras()');
-
-    //Sanity Check
-    if (!(window.google && window.google.maps && this.googleMap)) {
-      console.log('ERROR: MapExplorer.paintAllCameras() doesn\'t have a map to paint on!');
-      return;
-    }
-
-    let inputColour = '#f39';
-    let inputOpacity = 0.5;
-    let inputSize = 500;
-
-    Cameras.all.forEach(function(camera) {
-      let marker = new google.maps.Circle({
-        center: {
-          lat: parseFloat(camera.latitude),
-          lng: parseFloat(camera.longitude)
-        },
-        radius: inputSize,
-        fillColor: inputColour,
-        fillOpacity: inputOpacity,
-        strokeColor: '#fff',
-        strokeWeight: 0,
-        map: this.googleMap,
-        camera: camera
-      });
-
-
-      marker.addListener('click', function() {
-        alert(this.camera.ID);
-        this.map.setCenter(this.getCenter());
-        this.map.setZoom(12);
-      }.bind(marker));
-
-    }.bind(this));
   }
-
 });
