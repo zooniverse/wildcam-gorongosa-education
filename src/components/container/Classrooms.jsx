8edd5ee33deb3f04c1a7ed38ec60e2fe856da542
Roger Hutchings <roger@hutchin.gs>
2016-02-11 11:59:28 +0000

    Remove fetch

--- src/components/container/Classrooms.jsx
+++ src/components/container/Classrooms.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import fetch from 'isomorphic-fetch';
 
 import { eduAPI } from '../../config.json';
 import { fetchClassrooms } from '../../store/actions/classrooms';
