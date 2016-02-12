13fbdecd8f9a222ad8dc8a47476a2e2b5811e584
Simone Duca <simone@zooniverse.org>
2016-02-12 11:50:41 +0000

    For some yet obscure reason Rog version of createStore was throwing .

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers'
 
-import rootReducer from '../reducers';
-
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware,
+  createLogger()
+)(createStore)
 
 export default function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunkMiddleware, createLogger())
-  );
+  const store = createStoreWithMiddleware(rootReducer, initialState)
 
-  // Enable Webpack hot module replacement for reducers
   if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers');
-      store.replaceReducer(nextRootReducer);
+      const nextRootReducer = require('../reducers')
+      store.replaceReducer(nextRootReducer)
     })
   }
 
-  return store;
+  return store
 }
