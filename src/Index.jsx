4e2d5ed2e7dd3a5d964f764f219a9024d540310e
Jim O'Donnell <jim@zooniverse.org>
2016-03-23 13:31:56 +0000

    Create a teachers resources page

--- src/Index.jsx
+++ src/Index.jsx
@@ -15,6 +15,7 @@ import Home from './presentational/Home.jsx';
 import ClassroomsOverview from './presentational/ClassroomsOverview.jsx';
 import NewClassroomForm from './presentational/NewClassroomForm.jsx';
 import StudentOverview from './presentational/StudentOverview.jsx';
+import Resources from './presentational/Resources.jsx'
 
 import LoginPromptPage from './presentational/LoginPromptPage.jsx';
 import ErrorPage from './presentational/ErrorPage.jsx';
@@ -44,6 +45,7 @@ oauth.init(panoptesAppId)
                 <Route path=":classroomId" component={Classroom} />
               </Route>
               <Route path="data" component={MapExplorer} />
+              <Route path="resources" component={Resources} />
             </Route>
             <Route path="students" component={Students}>
               <IndexRedirect to="classrooms" />
