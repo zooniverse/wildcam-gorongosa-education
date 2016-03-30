43cd38fb9b1d37358f8651fe0a326d4df7a26d49
Jim O'Donnell <jim@zooniverse.org>
2016-03-30 16:27:42 +0100

    updated text for the home page

--- src/presentational/Home.jsx
+++ src/presentational/Home.jsx
@@ -5,9 +5,7 @@ import Panoptes from 'panoptes-client';
 import Layout from './Layout.jsx'
 
 
-export default class Home extends Component {
-
-  render() {
+const Home = (props) => {
     return (
       <Layout>
         <div className='home'>
@@ -16,8 +14,8 @@ export default class Home extends Component {
                 <h1>Welcome to the WildCam Lab</h1>
               </div>
               <div className='sub-title'>
-                <p>Are you an educator or an explorer?</p>
-                <p>Choose and get started!</p>
+                <p>Investigate ecological questions by exploring trail camera data using an interactive map. Filter and download data to perform analyses and test hypotheses. If you are an educator, you can set up private classrooms and invite your students to join. Curate data sets or let your students explore on their own. Guided activities and supporting educational resources are also available. If you are not an educator, you can simply explore the data and look for trends.</p>
+                <p>Are you an educator or an explorer? Make your selection to get started!</p>
                 <Link className='btn btn-primary btn-lg' role='button' to='/teachers'>Educator </Link>
                 &nbsp;&nbsp;
                 <Link className='btn btn-primary btn-lg' role='button' to='/students'>Explorer</Link>
@@ -28,4 +26,4 @@ export default class Home extends Component {
     );
   }
 
-}
+export {Home as default}
