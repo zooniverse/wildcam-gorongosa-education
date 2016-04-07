af27251404fae0af1aeed59a001fc3dc23b6fcb3
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-04-07 14:25:31 +0100

    Fix 144 and 169 in one shot

--- src/containers/Students.jsx
+++ src/containers/Students.jsx
@@ -1,13 +1,17 @@
 import { Component } from 'react';
 
-import Layout from '../presentational/Layout.jsx'
-
+import Layout from '../presentational/Layout.jsx';
 
 export default class Students extends Component {
 
   render() {
+    let loginSecured = this.props.navItems.reduce((prev, item, index, arr) => {  //Is this particular sub-page login secured?
+      console.log(prev, item.loginSecured, this.props.location.pathname.toLowerCase().startsWith(item.to), item.to);
+      return prev || (item.loginSecured === true && this.props.location.pathname.toLowerCase().startsWith(item.to));
+    }, false);
+    
     return (
-      <Layout {...this.props} navItems={this.props.navItems}>
+      <Layout {...this.props} loginSecured={loginSecured} navItems={this.props.navItems}>
         {this.props.children}
       </Layout>
     );
@@ -20,6 +24,7 @@ Students.defaultProps = {
     {
       label: 'Classrooms',
       to: '/students/classrooms',
+      loginSecured: true,
     },
     {
       label: 'Data',
