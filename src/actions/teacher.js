e009cda65abfc5f8180326c10dfe674643a88307
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-05-19 18:08:08 +0100

    References to features in panoptes-client have been updated

--- src/actions/teacher.js
+++ src/actions/teacher.js
@@ -1,6 +1,6 @@
 import { browserHistory } from 'react-router';
 import fetch from 'isomorphic-fetch';
-import Panoptes from 'panoptes-client';
+import apiClient from 'panoptes-client/lib/api-client';
 
 import config from '../constants/config';
 import * as types from '../constants/actionTypes';
@@ -21,7 +21,7 @@ export function createClassroom(name, subject, school, description) {
       method: 'POST',
       mode: 'cors',
       headers: new Headers({
-          'Authorization': Panoptes.apiClient.headers.Authorization,
+          'Authorization': apiClient.headers.Authorization,
           'Content-Type': 'application/json'
       }),
       body: JSON.stringify({
@@ -57,7 +57,7 @@ export function fetchClassrooms() {
       method: 'GET',
       mode: 'cors',
       headers: new Headers({
-          'Authorization': Panoptes.apiClient.headers.Authorization,
+          'Authorization': apiClient.headers.Authorization,
           'Content-Type': 'application/json'
         })
       })
