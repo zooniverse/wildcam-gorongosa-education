ea2dcb6972716377c62a91ea43e64af931b745ad
Roger Hutchings <roger@hutchin.gs>
2016-07-20 17:36:35 +0100

    Basic student assignments page

---
+++ src/modules/students/components/ClassroomAssignments.jsx
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+
+const ClassroomAssignments = props => {
+  console.log('props', props);
+  const { data } = props;
+  return (
+    <div className="student-assignmentlist">
+      <h1>Assignments!</h1>
+      { data.map(classroom =>
+        <section key={ classroom.classroom_id }>
+          <h3>{ classroom.classroom_name }</h3>
+          <ul className="list">
+            { classroom.assignments.map(assignment =>
+              <li key={ assignment.id } className="listitem">
+                <div>{ assignment.name }</div>
+                <div>
+                  <a className="btn btn-default"
+                    href={`https://www.wildcamgorongosa.org/#/classify/assignment-${ assignment.id }`}
+                    target="_blank"
+                    role="button">
+                    Start assignment
+                  </a>
+                </div>
+              </li>
+            ) }
+          </ul>
+        </section>
+      ) }
+    </div>
+  );
+};
+
+export default ClassroomAssignments;
---
+++ src/modules/students/containers/StudentAssignmentsContainer.jsx
@@ -0,0 +1,65 @@
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import ClassroomAssignments from '../components/ClassroomAssignments';
+import { fetchStudentClassrooms, fetchStudentAssignments } from '../actions';
+
+
+class StudentAssignmentsContainer extends Component {
+
+  constructor() {
+    super();
+    this.createClassroomAssignmentData = this.createClassroomAssignmentData.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.fetchStudentClassrooms();
+    this.props.fetchStudentAssignments();
+  }
+
+  createClassroomAssignmentData() {
+    const assignments = this.props.assignments.data;
+    const classrooms = this.props.classrooms.data;
+
+    return classrooms.reduce((result, classroom) => {
+      const assignmentsForClassroom = assignments.filter(assignment =>
+        classroom.id === assignment.attributes.classroom_id.toString());
+
+      if (assignmentsForClassroom.length) {
+        result.push({
+          classroom_id: classroom.id,
+          classroom_name: classroom.attributes.name,
+          assignments: assignmentsForClassroom.map(assignment => ({
+            id: assignment.id,
+            name: assignment.attributes.name,
+          }))
+        });
+      }
+
+      return result;
+    }, []);
+  }
+
+  render() {
+    const classroomData = this.createClassroomAssignmentData();
+    const { assignments, classrooms } = this.props;
+    return classrooms.loading || assignments.loading
+      ? <div>Loading assignments...</div>
+      : <ClassroomAssignments data={ classroomData } />;
+  }
+
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+    fetchStudentClassrooms,
+    fetchStudentAssignments
+  }, dispatch);
+
+const mapStateToProps = state => ({
+  classrooms: state.student.classrooms,
+  assignments: state.assignment.assignments,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StudentAssignmentsContainer);
