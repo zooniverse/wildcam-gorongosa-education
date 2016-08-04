cf9758f9c88368b0290f3f864122f925806b985a
Simone Duca <simone@zooniverse.org>
2016-08-04 09:45:57 +0100

    Improve styling of students assignments view.

--- src/modules/students/components/ClassroomAssignments.jsx
+++ src/modules/students/components/ClassroomAssignments.jsx
@@ -4,40 +4,39 @@ const ClassroomAssignments = props => {
   const { data } = props;
   return (
     <div className="student-assignmentlist">
-      <h1>Assignments!</h1>
+      <h1>Assignments</h1>
       { data.map(classroom =>
         <section key={ classroom.classroom_id }>
           <h3>Classroom { classroom.classroom_name }</h3>
-          <ul className="list">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Due date</th>
+                <th>Progress</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
             { classroom.assignments.map(assignment =>
-              <li key={ assignment.id } className="listitem">
-                <div>
-                  <div><strong>Name</strong></div>
-                  <div>{ assignment.name }</div>
-                </div>
-                <div>
-                  <div><strong>Description</strong></div>
-                  <div>{ assignment.description }</div>
-                </div>
-                <div>
-                  <div><strong>Due date</strong></div>
-                  <div>{ assignment.duedate }</div>
-                </div>
-                <div>
-                  <div><strong>Progress</strong></div>
-                  <div>{ assignment.classification_count }/{ assignment.target }</div>
-                </div>
-                <div>
-                  <a className="btn btn-default"
+              <tr key={ assignment.id } className="listitem">
+                <td>{ assignment.name }</td>
+                <td>{ assignment.description }</td>
+                <td>{ assignment.duedate }</td>
+                <td>{ assignment.classification_count }/{ assignment.target }</td>
+                <td>
+                  <a className="btn btn-primary"
                     href={`https://www.wildcamgorongosa.org/#/classify/assignment-${ assignment.id }`}
                     target="_blank"
                     role="button">
                     Start assignment
                   </a>
-                </div>
-              </li>
+                </td>
+              </tr>
             ) }
-          </ul>
+            </tbody>
+          </table>
         </section>
       ) }
     </div>
