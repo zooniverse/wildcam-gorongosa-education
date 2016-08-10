1d5fed90e797834191f6ad39003b5afe0bc00ff1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-08-10 12:43:02 +0100

    Realtime filter updates on the map!

--- src/modules/maps/containers/MapVisuals.jsx
+++ src/modules/maps/containers/MapVisuals.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Script } from 'react-loadscript';
 import { initialState } from '../../../reducers/mapexplorer';
 
-const config = require('../../../constants/mapExplorer.config.json');
+const mapconfig = require('../../../constants/mapExplorer.config.json');
 const gorongosaGeodata = require('../../../map-data/gorongosa-geodata.json');
 const vegetationGeodata = require('../../../map-data/vegetation-geodata.json');
 
@@ -47,7 +47,7 @@ class MapVisuals extends Component {
   initMapExplorer() {
     //Req check
     if (!(window.L && window.cartodb)) {
-      console.log('MapExplorer.initMapExplorer(): failed');
+      console.error('MapExplorer.initMapExplorer(): failed');
       return;
     }
     
@@ -62,7 +62,7 @@ class MapVisuals extends Component {
     //Prepare the base layers.
     let baseLayers = [];
     let baseLayersForControls = {};
-    config.baseLayers.map((layer) => {
+    mapconfig.baseLayers.map((layer) => {
       const newLayer = L.tileLayer(layer.url, {
         attribution: layer.attribution
       });
@@ -72,8 +72,8 @@ class MapVisuals extends Component {
 
     //Go go gadget Leaflet Map!
     this.map = new L.Map('mapVisuals', {  //Leaflet 0.7.7 comes with cartodb.js 3.15
-      center: [config.mapCentre.latitude, config.mapCentre.longitude],
-      zoom: config.mapCentre.zoom,
+      center: [mapconfig.mapCentre.latitude, mapconfig.mapCentre.longitude],
+      zoom: mapconfig.mapCentre.zoom,
       layers: baseLayers[0]  //Set the default base layer
     });
     
@@ -92,31 +92,33 @@ class MapVisuals extends Component {
       }).addTo(this.map),
     };
     
-    //Create the CartoDB Data layer
-    cartodb.createLayer(this.map, config.cartodb.vizUrl)
-    .done((layer) => {
-      layer.addTo(this.map);
-      this.cartodbLayer = layer;
-      this.cartodbLayer.setInteraction(true);
-      layer.on('error', (err) => {
-        console.error('ERROR (initMapExplorer(), cartodb.createLayer().on(\'done\')): ' + err);
-      });
-
-      //Add the controls for the layers
-      L.control.layers(baseLayersForControls, {
-        'Data': layer,
-        ...geomapLayers
-      }, {
-        position: 'topleft',
-        collapsed: true,
-      }).addTo(this.map);
-
-      //updateDataVisualisation performs some cleanup
-      this.updateDataVisualisation(this.props);
-    })
-    .error((err) => {
-      console.error('ERROR (initMapExplorer(), cartodb.createLayer()):' + err);
-    });
+    //Create the CartoDB Data Layer
+    this.cartodbLayer = L.geoJson(null, {
+      pointToLayer: function (feature, latlng) {
+        const MINRADIUS = 5;
+        const MAXRADIUS = 25;
+        const fillColor = (feature.properties.count > 0)
+          ? '#fc3' : '#333';
+        let radius = feature.properties.count || 0;
+        radius = 5 + radius / 100;
+        radius = Math.min(Math.max(radius, MINRADIUS), MAXRADIUS);
+        
+        const marker = L.circleMarker(latlng, {
+          color: '#fff',
+          fillColor: fillColor,
+          fillOpacity: 0.8,
+          radius: radius,
+        });
+        //TEST
+        //----------------
+        marker.on('click', (e) => {
+          alert('CLICK!');
+        });
+        //----------------
+        return marker;
+      }
+    }).addTo(this.map);
+    this.updateDataVisualisation(this.props);    
 
     //Bonus: 'Recentre Map' button
     const recentreButton = L.control({position: 'topleft'});
@@ -129,6 +131,15 @@ class MapVisuals extends Component {
       return container;
     }
     recentreButton.addTo(this.map);
+    
+    //Bonus: Add the Layer Controls
+    L.control.layers(baseLayersForControls, {
+      'Data': this.cartodbLayer,
+      ...geomapLayers
+    }, {
+      position: 'topleft',
+      collapsed: true,
+    }).addTo(this.map);
     //--------------------------------
 
     //Cleanup then go
@@ -139,16 +150,48 @@ class MapVisuals extends Component {
   
   recentreMap() {
     if (!this.map) return;
-    this.map.setZoom(config.mapCentre.zoom);
-    this.map.panTo([config.mapCentre.latitude, config.mapCentre.longitude]);
+    this.map.setZoom(mapconfig.mapCentre.zoom);
+    this.map.panTo([mapconfig.mapCentre.latitude, mapconfig.mapCentre.longitude]);
   }
   
   updateDataVisualisation(props = this.props) {
+    
+    console.log('-'.repeat(80));
+    console.log(props.mapexplorer.species);
+    
     //Req check
     if (!(this.map && this.cartodbLayer)) {
-      console.log('MapVisuals.updateDataVisualisation(): failed');
+      console.error('MapVisuals.updateDataVisualisation(): failed');
       return;
     }
+    
+    let sqlWhere = '';
+    if (props.mapexplorer.species) {
+      sqlWhere = props.mapexplorer.species.map((id) => {
+        let val = mapconfig.species.find(ele => { return ele.id === id });
+        val = (val) ? val.dbName : '';
+        return ' species = \''+val+'\' ';
+      }).join(' OR ');
+    }
+    if (sqlWhere !== '') { sqlWhere = ' WHERE ' + sqlWhere; }
+    
+    let sql = mapconfig.cartodb.sqlQueryCountItems
+      .replace('{CAMERAS}', mapconfig.cartodb.sqlTableCameras)
+      .replace('{SUBJECTS}', mapconfig.cartodb.sqlTableSubjects)
+      .replace('{CLASSIFICATIONS}', mapconfig.cartodb.sqlTableClassifications)
+      .replace('{AGGREGATIONS}', mapconfig.cartodb.sqlTableAggregations)
+      .replace('{WHERE}', sqlWhere);
+    
+    console.log('SQL REQUEST: ', sql);
+    
+    let cartoSql = cartodb.SQL({user: 'shaunanoordin-zooniverse', format: 'geojson'});
+    cartoSql.execute(sql)
+    .done((geojson) => {
+      this.cartodbLayer.clearLayers();
+      this.cartodbLayer.addData(geojson);
+    });
+    
+    
   }
 }
 
