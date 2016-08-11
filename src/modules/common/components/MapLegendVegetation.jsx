e6990c5fc10cfca2464671fbd25f2f005cbc3b5f
Simone Duca <simone@zooniverse.org>
2016-08-11 15:28:29 +0100

    Add key to map legend and text to progress.

--- src/modules/common/components/MapLegendVegetation.jsx
+++ src/modules/common/components/MapLegendVegetation.jsx
@@ -3,11 +3,11 @@ const vegetationGeodata = require('../../../map-data/vegetation-geodata.json');
 
 const MapLegendVegetation = (props) => {
   let keys = [];
-  
+
   for (let key in vegetationGeodata.specificStyles) {
     const color = vegetationGeodata.specificStyles[key].color;
     keys.push(
-      <div>
+      <div key={key}>
         <svg height="10" width="10"><circle cx="5" cy="5" r="5" fill={color} fillOpacity="0.5" /></svg> : {key}
       </div>);
   }
