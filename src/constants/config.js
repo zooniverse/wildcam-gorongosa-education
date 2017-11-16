f3039820df07f84ed5a7827049ad48c4162ae81d
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-11-16 15:58:14 +0000

    Update config file to support env switches

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,25 @@
+/*
+Configuration Settings
+----------------------
+
+The config settings change depending on which environment the app is running in.
+By default, this is the development environment, but this can be changed either by:
+
+- An env query string, e.g. localhost?env=production
+  (This changes the Panoptes JS Client does)
+- The NODE_ENV environment variable on the system running the app.
+
+ */
+
+var DEFAULT_ENV = 'development';
+var envFromBrowser = locationMatch(/\W?env=(\w+)/);
+var envFromShell = process.env.NODE_ENV;
+var env = envFromBrowser || envFromShell || DEFAULT_ENV;
+
+if (!env.match(/^(production|staging|development)$/)) {
+  throw new Error(`Error: Invalid Environment - ${envFromShell}`);
+}
+
 import { mergeDeep } from '../helpers';
 
 const defaultConfig = {
@@ -39,5 +61,17 @@ const envConfig = {
     panoptesReturnUrl: 'https://lab.wildcamgorongosa.org/'
   }
 };
+envConfig.development = envConfig.staging;
 
-export default mergeDeep(defaultConfig, envConfig[process.env.NODE_ENV]);
+export default mergeDeep(defaultConfig, envConfig[env]);
+export { env };
+
+// Try and match the location.search property against a regex. Basically mimics
+// the CoffeeScript existential operator, in case we're not in a browser.
+function locationMatch(regex) {
+  var match;
+  if (typeof location !== 'undefined' && location !== null) {
+    match = location.search.match(regex);
+  }
+  return (match && match[1]) ? match[1] : undefined;
+}
