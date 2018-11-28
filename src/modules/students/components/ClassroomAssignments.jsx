d5321a0f9877351672089d9d7b57970fc6534a4c
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-11-28 16:37:41 +0000

    Add notice for students to login at www.wildcamgorongosa.org

--- src/modules/students/components/ClassroomAssignments.jsx
+++ src/modules/students/components/ClassroomAssignments.jsx
@@ -41,6 +41,9 @@ class ClassroomAssignments extends Component {
     return (
     <div className="student-assignmentlist">
       <h1>Assignments</h1>
+      <div className="notice">
+        Please make sure that you first Sign In at <a href="https://www.wildcamgorongosa.org/" target="_blank" rel="noreferrer noopener">www.wildcamgorongosa.org</a> with the same account before starting an Assignment.
+      </div>
       { data.map(classroom =>
         <section key={ classroom.classroom_id }>
           <h3>Classroom { classroom.classroom_name }</h3>
